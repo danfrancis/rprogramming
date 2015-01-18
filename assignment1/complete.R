99b853eff7544196ddd5aa441996009aa89c8bf0
Daniel Francis <francis.daniel@gmail.com>
2015-01-17 23:34:50 -0800

    programming assignment 1-3

--- assignment1/complete.R
+++ assignment1/complete.R
@@ -1,7 +1,7 @@
 complete <- function(directory, id = 1:332) {
     nobs <- NULL
     for(i in id) {
-        filename = paste(directory,"/",formatC(i, width=3, flag="0"),".csv",sep="")
+        filename <- paste(directory,"/",formatC(i,width=3,flag="0"),".csv",sep="")
         filedata <- read.csv(filename)
         nobs <- append(nobs, sum(complete.cases(filedata)))
     }
--- assignment1/pollutantmean.R
+++ assignment1/pollutantmean.R
@@ -1,7 +1,7 @@
 pollutantmean <- function(directory, pollutant, id = 1:332) {
     allobs <- NULL
     for(i in id) {
-        filename = paste(directory,"/",formatC(i, width=3, flag="0"),".csv",sep="")
+        filename <- paste(directory,"/",formatC(i,width=3,flag="0"),".csv",sep="")
         filedata <- read.csv(filename)
         allobs = append(allobs, filedata[[pollutant]])
     }
